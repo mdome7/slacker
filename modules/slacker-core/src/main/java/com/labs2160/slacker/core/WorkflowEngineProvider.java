2ae5943081cec79a094f57e49b1a5518c9c6745f
mdome7 <mdome7@gmail.com>
2015-03-20 17:15:24 -0700

    Path to slacker.properties defaults to working dir or can be spcified using 'config' system property

--- modules/slacker-core/src/main/java/com/labs2160/slacker/core/WorkflowEngineProvider.java
+++ modules/slacker-core/src/main/java/com/labs2160/slacker/core/WorkflowEngineProvider.java
@@ -28,58 +28,60 @@ import com.labs2160.slacker.plugin.weather.WeatherAction;
  */
 @ApplicationScoped
 public class WorkflowEngineProvider {
-	private static final Logger logger = LoggerFactory.getLogger(WorkflowEngineProvider.class);
+    private static final Logger logger = LoggerFactory.getLogger(WorkflowEngineProvider.class);
 
-	@Produces @ApplicationScoped @Named("engine")
-	public WorkflowEngine provide() throws IOException {
-		// TODO: inject config and explore other Configuration classes, format (YML)
-		Properties config = new Properties();
-		config.load(new FileInputStream("/app-data/slacker/slacker.properties"));
-		
-		final long start = System.currentTimeMillis();
-		WorkflowEngineImpl engine = new WorkflowEngineImpl();
-		initializeCollectors(engine, config);
-		initializeWorkflows(engine, config);
-		logger.debug("Engine initialized in {} ms", System.currentTimeMillis() - start);
-		return engine;
-	}
-	
-	private void initializeCollectors(WorkflowEngineImpl engine, Properties config) {
-		HipChatCollector hcListener = new HipChatCollector(config.getProperty("xmpp.host"),
-				config.getProperty("xmpp.user"), config.getProperty("xmpp.password"));
-		engine.addCollector("HipChat", hcListener);
-	}
-	
-	private void initializeWorkflows(WorkflowEngineImpl engine, Properties config) {
-		PrintToConsoleEndpoint p2c = new PrintToConsoleEndpoint();
-		
-		Workflow wfEcho = new Workflow("Echo", "Echoes messages back");
-		wfEcho.setArgsSpecification("<any string>");
-		wfEcho.setDescription("Echoes messages back");
-		wfEcho.addAction(new EchoAction());
-		wfEcho.addEndpoint(p2c);
-		engine.addWorkflow(wfEcho, "echo");
+    @Produces @ApplicationScoped @Named("engine")
+    public WorkflowEngine provide() throws IOException {
+        // TODO: inject config and explore other Configuration classes, format (YML)
+        Properties config = new Properties();
+        final String configPath = System.getProperty("config","slacker.properties");
+        logger.info("Loading config file: {}", configPath);
+        config.load(new FileInputStream(configPath));
 
-		Workflow wfStock = new Workflow("Stock Price", "Retrieves stock price for the given symbol");
-		wfStock.setArgsSpecification("<stock symbol>");
-		wfStock.addAction(new MarkitStockAction());
-		wfStock.addEndpoint(p2c);
-		engine.addWorkflow(wfStock, "stock");
-		engine.addWorkflow(wfStock, "stock", "price");
+        final long start = System.currentTimeMillis();
+        WorkflowEngineImpl engine = new WorkflowEngineImpl();
+        initializeCollectors(engine, config);
+        initializeWorkflows(engine, config);
+        logger.debug("Engine initialized in {} ms", System.currentTimeMillis() - start);
+        return engine;
+    }
 
-		Workflow wfWeather = new Workflow("Weather", "Gets the current weather for the specified location");
-		wfWeather.setArgsSpecification("<location or zip code>");
-		wfWeather.setExampleArgs("\"Seattle, WA\" or 98012");
-		wfWeather.addAction(new WeatherAction());
-		wfWeather.addEndpoint(p2c);
-		engine.addWorkflow(wfWeather, "weather");
+    private void initializeCollectors(WorkflowEngineImpl engine, Properties config) {
+        HipChatCollector hcListener = new HipChatCollector(config.getProperty("xmpp.host"),
+                config.getProperty("xmpp.user"), config.getProperty("xmpp.password"));
+        engine.addCollector("HipChat", hcListener);
+    }
 
-		Workflow wfRandomPicker = new Workflow("Random Picker", "Picks random values from a set");
-		wfRandomPicker.setArgsSpecification("<num to pick> <choice 1> <choice 2> ... <choice N>");
-		wfRandomPicker.setExampleArgs("2 apple banana orange");
-		wfRandomPicker.addAction(new RandomPickerAction());
-		wfRandomPicker.addEndpoint(p2c);
-		engine.addWorkflow(wfRandomPicker, "pick");
-	}
-	
+    private void initializeWorkflows(WorkflowEngineImpl engine, Properties config) {
+        PrintToConsoleEndpoint p2c = new PrintToConsoleEndpoint();
+
+        Workflow wfEcho = new Workflow("Echo", "Echoes messages back");
+        wfEcho.setArgsSpecification("<any string>");
+        wfEcho.setDescription("Echoes messages back");
+        wfEcho.addAction(new EchoAction());
+        wfEcho.addEndpoint(p2c);
+        engine.addWorkflow(wfEcho, "echo");
+
+        Workflow wfStock = new Workflow("Stock Price", "Retrieves stock price for the given symbol");
+        wfStock.setArgsSpecification("<stock symbol>");
+        wfStock.addAction(new MarkitStockAction());
+        wfStock.addEndpoint(p2c);
+        engine.addWorkflow(wfStock, "stock");
+        engine.addWorkflow(wfStock, "stock", "price");
+
+        Workflow wfWeather = new Workflow("Weather", "Gets the current weather for the specified location");
+        wfWeather.setArgsSpecification("<location or zip code>");
+        wfWeather.setExampleArgs("\"Seattle, WA\" or 98012");
+        wfWeather.addAction(new WeatherAction());
+        wfWeather.addEndpoint(p2c);
+        engine.addWorkflow(wfWeather, "weather");
+
+        Workflow wfRandomPicker = new Workflow("Random Picker", "Picks random values from a set");
+        wfRandomPicker.setArgsSpecification("<num to pick> <choice 1> <choice 2> ... <choice N>");
+        wfRandomPicker.setExampleArgs("2 apple banana orange");
+        wfRandomPicker.addAction(new RandomPickerAction());
+        wfRandomPicker.addEndpoint(p2c);
+        engine.addWorkflow(wfRandomPicker, "pick");
+    }
+
 }
