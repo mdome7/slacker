2d71fdbc00fcd8145ddd23442f734e279b94f446
Kyle Steiner <kyleste@live.com>
2016-01-27 15:10:06 -0800

    Updated scheduler to use Cron4j

--- modules/slacker-plugin-extra/src/main/java/com/labs2160/slacker/plugin/extra/ScheduleTrigger.java
+++ modules/slacker-plugin-extra/src/main/java/com/labs2160/slacker/plugin/extra/ScheduleTrigger.java
@@ -1,72 +1,46 @@
 package com.labs2160.slacker.plugin.extra;
 
-import com.labs2160.slacker.api.Trigger;
 import com.labs2160.slacker.api.RequestHandler;
 import com.labs2160.slacker.api.SlackerRequest;
 import com.labs2160.slacker.api.SlackerResponse;
-
-import java.util.concurrent.Future;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.Properties;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.Executors;
-import java.util.TimerTask;
+import com.labs2160.slacker.api.Trigger;
+import it.sauronsoftware.cron4j.Scheduler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.Future;
 
 public class ScheduleTrigger implements Trigger {
 
     private static final Logger logger = LoggerFactory.getLogger(Trigger.class);
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private static final Scheduler scheduler = new Scheduler();
 
-    private String interval;
-    private String[] requests;
-
-    public ScheduleTrigger() { }
+    private String schedulingPattern;
+    private String request;
 
     @Override
     public void setConfiguration(Properties config) {
-        this.interval = config.getProperty("interval");
-        this.requests = config.getProperty("requests").split(";");
+        this.schedulingPattern = config.getProperty("schedulingPattern");
+        this.request = config.getProperty("request");
     }
 
     @Override
-    public void start(RequestHandler handler) {
-        TimerTask tt = new TriggerTask(handler);
-        TimeUnit tu;
-        switch (interval) {
-            case "second": tu = TimeUnit.SECONDS; break;
-            case "minute": tu = TimeUnit.MINUTES; break;
-            case "hour": tu = TimeUnit.HOURS; break;
-            case "day": tu = TimeUnit.DAYS; break;
-            default: tu = TimeUnit.MILLISECONDS; break;
-        }
-        scheduler.scheduleAtFixedRate(tt, 0, 1, tu);
-    }
-
-    private class TriggerTask extends TimerTask {
-        private RequestHandler handler;
-
-        public TriggerTask(RequestHandler handler) {
-            this.handler = handler;
-        }
-
-        @Override
-        public void run() {
-            logger.info("Trigger interval {} breakpoint reached. Starting its workflow now.", interval);
-            for (String request : requests) {
+    public void start(final RequestHandler handler) {
+        scheduler.schedule(schedulingPattern, new Runnable() {
+            public void run() {
+                logger.info("Trigger breakpoint reached. Processing request '{}' now.", request);
                 try {
-                    logger.info("Attempting request {}", request);
                     String[] tokens = request.split(" ");
                     SlackerRequest sr = new SlackerRequest("trigger", tokens);
                     Future<SlackerResponse> future = handler.handle(sr);
                     SlackerResponse response = future.get();
                     logger.info("Request {} succeeded with response {}", request, response);
                 } catch (Exception e) {
-                    logger.error("Could not run trigger due to error {}.", e);
+                    logger.error("Could not process request '{}' due to error {}.", request, e);
                 }
             }
-            logger.info("Trigger completed");
-        }
+        });
+        scheduler.start();
     }
 }
